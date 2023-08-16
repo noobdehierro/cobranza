ab33457f48f8ff077ffb47d9e0ded61defff1837
noobdehierro <joseluisreyesalvarez97@gmail.com>
2023-08-16 14:59:05 -0600

    more images

--- sw.js
+++ sw.js
@@ -11,6 +11,13 @@ const APP_SHELL = [
   "css/style.css",
   "images/favicon.ico",
   "images/icons/icon-72x72.png",
+  "images/icons/icon-96x96.png",
+  "images/icons/icon-128x128.png",
+  "images/icons/icon-144x144.png",
+  "images/icons/icon-152x152.png",
+  "images/icons/icon-192x192.png",
+  "images/icons/icon-384x384.png",
+  "images/icons/icon-512x512.png",
   "js/app.js",
   "js/sw-utils.js",
 ];
