fb73f58741e9103f08353f66bff0825c01ad8cad
noobdehierro <joseluisreyesalvarez97@gmail.com>
2023-08-24 16:59:11 -0600

    avance

--- js/app.js
+++ js/app.js
@@ -41,6 +41,10 @@ function registerServiceWorker() {
 registerServiceWorker();
 
 $(document).ready(function () {
+  $("#telefono").mask("0000000000");
+  $("#celular").mask("0000000000");
+  $("#telefonoContacto").mask("0000000000");
+
   $("#confirmCode").click(function () {
     var code = $("#code").val();
 
@@ -52,22 +56,26 @@ $(document).ready(function () {
       $.ajax({
         showLoader: true,
         type: "GET",
-        url: "https://jsonplaceholder.typicode.com/todos/1",
+        // url: "https://jsonplaceholder.typicode.com/todos/1",
+        url: "http://serverpwa.test/api/pwa",
         data: {
           code: code,
         },
-        success: function (data) {
+        success: function ({ data }) {
           console.log(data);
 
-          let clientName = {
-            name: "Jorge Leon",
-            adeudo: 50000,
-          };
+          // let clientName = {
+          //   name: "Jorge Leon",
+          //   adeudo: 50000,
+          // };
 
           $("#welcome").hide();
 
-          $(".clientName").text(clientName.name);
-          $(".totalMont").text(clientName.adeudo);
+          // $(".clientName").text(clientName.name);
+          // $(".totalMont").text(clientName.adeudo);
+
+          $(".clientName").text(data.name);
+          $(".totalMont").text(data.adeudo);
           showquestion("question1");
         },
         error: function (error) {
@@ -81,8 +89,12 @@ $(document).ready(function () {
     }
   });
 
-  $("#button-1, #button-2").click(function () {
+  $("#button-1").click(function () {
     showquestion("question1-1");
+  });
+
+  $("#prevHome").click(function () {
+    showquestion("confirmCode");
   });
 
   $("#button-1-1").click(function () {
@@ -91,6 +103,42 @@ $(document).ready(function () {
 
   $("#button-1-2").click(function () {
     showquestion("question1-1-2");
+  });
+
+  $("#button-2").click(function () {
+    showquestion("question2-1");
+  });
+
+  $("#help").click(function (e) {
+    e.preventDefault();
+    var celular = $("#celular").val(),
+      email = $("#email").val(),
+      telefono = $("#telefono").val(),
+      telefonoContacto = $("#telefonoContacto").val();
+
+    if ($("#helpForm").valid()) {
+      if (celular || telefono || email || telefonoContacto) {
+        console.log(celular, email, telefono, telefonoContacto);
+
+        $.ajax({
+          type: "get",
+          // url: "http://serverpwa.test/api/pwa",
+          url: "https://jsonplaceholder.typicode.com/todos/1",
+          data: {
+            code: 123456,
+          },
+          success: function (response) {
+            showquestion("thankYou");
+          },
+        });
+      } else {
+        Swal.fire({
+          icon: "info",
+          title: "Ayudalo",
+          text: "llena alguno de los campos, y lo podras apoyar con mas facilidad",
+        });
+      }
+    }
   });
 
   function showquestion(questionId) {
@@ -112,5 +160,39 @@ $(document).ready(function () {
 
   $("#creditCard").click(function () {
     showquestion("creditCardForm");
+  });
+
+  $("#helpForm").validate({
+    rules: {
+      celular: {
+        minlength: 10,
+        digits: true,
+      },
+      telefono: {
+        minlength: 10,
+        digits: true,
+      },
+      telefonoContacto: {
+        minlength: 10,
+        digits: true,
+      },
+    },
+    messages: {
+      celular: {
+        minlength: "El número de celular debe tener al menos 10 caracteres.",
+        maxlength: "El número de celular no debe exceder los 10 caracteres.",
+        digits: "Ingresa solo dígitos en este campo.",
+      },
+      telefono: {
+        minlength: "El número de teléfono debe tener al menos 10 caracteres.",
+        maxlength: "El número de teléfono no debe exceder los 10 caracteres.",
+        digits: "Ingresa solo dígitos en este campo.",
+      },
+      telefonoContacto: {
+        minlength: "El número de teléfono debe tener al menos 10 caracteres.",
+        maxlength: "El número de teléfono no debe exceder los 10 caracteres.",
+        digits: "Ingresa solo dígitos en este campo.",
+      },
+    },
   });
 });
